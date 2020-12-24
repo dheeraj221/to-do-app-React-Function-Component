ac9934ff1b9ebe6e354bddb078262eddc9262537
dheeraj221 <dpatel@gammastack.com>
2020-12-24 18:40:58 +0530

    use hook with functional component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import ListItems from "./ListItems";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: [],
-      currentItem: {
-        text: "",
-        key: "",
-      },
-    };
-  }
+function App() {
+  const initialValue = {
+    items: [],
+    currentItem: {
+      text: "",
+    },
+  };
 
-  handleInput = (e) => {
-    this.setState({
+  const [state, setValue] = useState(initialValue);
+
+  const handleInput = (e) => {
+    setValue({
+      ...state,
       currentItem: {
         text: e.target.value,
         key: Date.now(),
@@ -23,73 +22,64 @@ class App extends Component {
     });
   };
 
-  addItem = (e) => {
+  const addItem = (e) => {
     e.preventDefault();
-    const newItem = this.state.currentItem;
-    if (newItem.text !== "") {
-      const items = [...this.state.items, newItem];
-      this.setState({
-        items: items,
-        currentItem: {
-          text: "",
-          key: "",
-        },
-      });
-    }
-  };
-
-  deleteItem = (key) => {
-    const filteredItems = this.state.items.filter((item) => item.key !== key);
-    this.setState({
-      items: filteredItems,
+    const items = [...state.items, state.currentItem];
+    setValue({
+      items: items,
+      currentItem: {
+        text: "",
+        key: "",
+      },
     });
   };
 
-  deleteAllItem = (e) => {
-    e.preventDefault();
-    this.setState({
-      items: [],
-    });
+  const deleteItem = (key) => {
+    const filteredItems = state.items.filter((item) => item.key !== key);
+    setValue({ ...state, items: filteredItems });
   };
 
-  updateItem = (text, key) => {
-    const items = this.state.items;
+  const deleteAllItem = (e) => {
+    e.preventDefault();
+    setValue({ ...state, items: [] });
+  };
+
+  const updateItem = (text, key) => {
+    const items = state.items;
     items.map((item) => {
       if (item.key === key) {
         item.text = text;
       }
       return item;
     });
-    this.setState({
+    setValue({
+      ...state,
       items: items,
     });
   };
 
-  render() {
-    return (
-      <div className="App">
-        <form id="to-do-form" onSubmit={this.addItem}>
-          <input
-            type="text"
-            placeholder="Enter task"
-            value={this.state.currentItem.text}
-            onChange={this.handleInput}
-          ></input>
-          <button type="submit" disabled={!this.state.currentItem.text}>
-            Add
-          </button>
-          <button onClick={this.deleteAllItem}>Remove All</button>
-        </form>
-        <p>{this.state.items.text}</p>
+  return (
+    <div className="App">
+      <form id="to-do-form" onSubmit={addItem}>
+        <input
+          type="text"
+          placeholder="Enter task"
+          value={state.currentItem.text}
+          onChange={handleInput}
+        ></input>
+        <button type="submit" disabled={!state.currentItem.text}>
+          Add
+        </button>
+        <button onClick={deleteAllItem}>Remove All</button>
+      </form>
 
-        <ListItems
-          items={this.state.items}
-          deleteItem={this.deleteItem}
-          updateItem={this.updateItem}
-        />
-      </div>
-    );
-  }
+      <ListItems
+        items={state.items}
+        deleteItem={deleteItem}
+        updateItem={updateItem}
+      />
+    </div>
+  );
 }
 
 export default App;
